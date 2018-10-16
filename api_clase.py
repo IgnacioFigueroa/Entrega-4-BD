438d17e07194b56bf638ae5de38dec6fff40dbe9
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-10-16 13:20:06 -0300

    IniciarSesion

--- api_clase.py
+++ api_clase.py
@@ -5,15 +5,17 @@ Created on Sun Oct 14 18:51:18 2018
 @author: sjfig
 """
 
-
-
-
 import psycopg2
+import IniciarSesion
+
 def imprimir(mensaje):
     print(mensaje)
 
-conn = psycopg2.connect(database="grupo3", user="grupo3", password = "2gKdbj", host="201.238.213.114", port="54321")
+
+conn = psycopg2.connect(database="grupo3", user="grupo3", password="2gKdbj", host="201.238.213.114", port="54321")
 
 cur = conn.cursor()
 
-#Menu
+# Menu
+imprimir("BIENVENIDO/A A LINKEDING")
+imprimir()
