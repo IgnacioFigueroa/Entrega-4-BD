b0be1ed2af74bdec064c90f70fafec29cbd0a3d9
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-10-16 13:15:33 -0300

    Funcion imprimir

--- api_clase.py
+++ api_clase.py
@@ -5,11 +5,15 @@ Created on Sun Oct 14 18:51:18 2018
 @author: sjfig
 """
 
+
+
+
 import psycopg2
+def imprimir(mensaje):
+    print(mensaje)
 
 conn = psycopg2.connect(database="grupo3", user="grupo3", password = "2gKdbj", host="201.238.213.114", port="54321")
 
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM usuario")
-rows = cur.fetchall()
+#Menu
