590351799e3e6b3387d2e3998d0c544784e1f456
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2018-10-19 16:02:28 -0300

    Cambio en cammelcase recuperar contraseña

--- Empresas.py
+++ Empresas.py
@@ -1,2 +1,19 @@
+from IO import *
+
+
 def MenuEmpresas(usuario, conn):
+    Imprimir("\nMis Empresas")
+    Imprimir("Seleccione una empresa, ingrese 0 para salir.")
+    MostrarMisEmpresas(usuario, conn)
     return
+
+
+def MostrarMisEmpresas(usuario, conn):
+    cur = conn.cursor()
+    empresas = cur.execute("SELECT e.nombre FROM administrador a, empresa e WHERE a.id_empresa = e.id AND a.correo_usuario = '{}';".format(usuario))
+    empresas = cur.fetchall()
+    i = 1
+    for empresa in empresas:
+        Imprimir("Nombre empresa: {}".format(empresa[0]))
+    return
+
