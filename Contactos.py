39ecb2caff4550c8fee744a8e10661377a8a292f
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-10-19 12:08:15 -0400

    Cambios

--- Contactos.py
+++ Contactos.py
@@ -1,4 +1,16 @@
 from IO import *
+#----------QUERYS----------
+CONTACTOS_USUARIO = 'SELECT todos.correo correo, todos.amigo amigo' \
+                    ' FROM ' \
+                    '(SELECT u.correo correo, COUNT(*) amigos, ' \
+                    ' CASE  WHEN u.correo = s.correo_usuario_emisor THEN s.correo_usuario_receptor ' \
+                    'WHEN u.correo = s.correo_usuario_receptor THEN s.correo_usuario_emisor ' \
+                    'END amigo ' \
+                    'FROM ' \
+                    'usuario u JOIN solicitud s ON (u.correo = s.correo_usuario_emisor OR u.correo = s.correo_usuario_receptor) ' \
+                    'WHERE  s.estado = \'aceptada\' ' \
+                    'GROUP BY u.correo, s.correo_usuario_receptor, s.correo_usuario_emisor ORDER BY amigos DESC) todos ' \
+                    'ORDER BY todos.correo DESC'
 def MenuContactos(usuario, conn):
     Imprimir("MENU DE CONTACTOS")
     Imprimir("Que deseas hacer? \n"
@@ -15,7 +27,7 @@ def MenuContactos(usuario, conn):
     return
 
 def VerContactos(usuario, conn):
-
+    
     return
 def AgregarContactos(usuario, conn):
 
