417728c7a4c3874c142c7af903a1339b5795e33f
= <=>
2018-10-19 13:54:57 -0300

    listo RecuperarContrasena

--- RecuperarContraseña.py
+++ RecuperarContraseña.py
@@ -1,5 +1,6 @@
-import time
+from datetime import date
 from IO import *
+
 def RecuperarContrasena(conn):
         cur = conn.cursor()
         cur.execute("select * from contrasena_antigua")
@@ -13,7 +14,19 @@ def RecuperarContrasena(conn):
         correo = input("Ingrese su correo electronico: ")
         while correo not in dicCorreoContra.keys():
             correo = input("Correo no valido, ingreselo nuevamente: ")
-        Imprimir("Su contrasena actual es:" + dicCorreoContra[correo])
+        Imprimir("Su contrasena actual es: " + dicCorreoContra[correo])
         contraNueva = input("Ingrese una contrasena nueva: ")
         while len(contraNueva) not in range(1, 31):
             contraNueva = input("Largo de la contrasena invalido, ingrese una contrasena nueva: ")
+
+        idNuevo = max(dicCorreoId.values()) + 1
+        fechaActual = date.today()
+
+        #print("Insertando en contrasena_antigua:\nid:",
+        #      idNuevo, "\ncorreo:", correo, "\ncontrasena nueva:",
+        #      contraNueva, "\nfecha actual:", fechaActual)
+
+        cur.execute("insert into contrasena_antigua(id, correo_usuario, contrasena, fecha_creacion) "
+                    "values({},'{}','{}','{}');".format(idNuevo, correo, contraNueva, fechaActual))
+        conn.commit()
+        cur.close()
