3e11a43fb4b7a94f2d1177d01675cd46a93975ba
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-10-30 13:39:25 -0400

    Comienzo clase E5

--- MenuEstadisticas.py
+++ MenuEstadisticas.py
@@ -1,2 +1,17 @@
+from IO import *
 def MenuEstadisticas(usuario, conn):
+    Imprimir("Que desea hacer?"
+             "\t (1) Ver calidad de contactos"
+             "\t (2) Ver cantidad de comentarios")
+    opcion = ValidarOpcion(range(1,3))
+    if opcion == 1:
+        CalidadContactos(usuario, conn)
+    elif opcion == 2:
+        CantidadComentarios(usuario, conn)
+    return
+
+
+def CalidadContactos(usuario, conn):
+    return
+def CantidadComentarios(usuario, conn):
     return
