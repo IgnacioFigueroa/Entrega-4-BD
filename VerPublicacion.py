9c5d384f1c70b5ee4cc33ceb1a43c1adb301620e
= <=>
2018-11-15 17:40:06 -0300

    avanzando en (2) (iii)

--- VerPublicacion.py
+++ VerPublicacion.py
@@ -1,6 +1,10 @@
 from IO import *
 import datetime
 
+import psycopg2
+conn = psycopg2.connect(database="grupo3", user="grupo3", password="2gKdbj", host="201.238.213.114", port="54321")
+
+
 crearPublicacionUsuario = "INSERT INTO publicacion (id,correo_usuario,texto,foto,link,estado,fecha,borrada) " \
                           "VALUES ({},'{}','{}','{}','{}','{}',TO_DATE('{}', 'DD/MM/YYYY'),{})"
 
@@ -29,7 +33,7 @@ def MenuVerPublicacion(usuario, conn):
             MisPublicaciones(usuario, conn)
         elif opcion == 3:
             Imprimir("Aqui van Otras Publicaciones")
-    return
+            OtrasPublicaciones(usuario, conn)
 
 def CrearPublicacionesUsuario(usuario, conn):
     cur = conn.cursor()
@@ -110,7 +114,7 @@ def MisPublicaciones(usuario, conn):
             conn.close()
             sys.exit()
 
-def Comentar(id_publicacion, conn):
+def Comentar(id_a_comentar, conn, opcion): # opcion puede ser "comentario" o "publicacion"
     pass
 
 def EliminarComentario(id_publicacion, conn):
@@ -122,6 +126,108 @@ def EditarPublicacion(id_publicacion, conn):
 def EliminarPublicacion(id_publicacion, conn):
     pass
 
+CONTACTOS_USUARIO = 'SELECT todos.amigo amigo' \
+                    ' FROM ' \
+                    '(SELECT u.correo correo, COUNT(*) amigos, ' \
+                    ' CASE  WHEN u.correo = s.correo_usuario_emisor THEN s.correo_usuario_receptor ' \
+                    'WHEN u.correo = s.correo_usuario_receptor THEN s.correo_usuario_emisor ' \
+                    'END amigo ' \
+                    'FROM ' \
+                    'usuario u JOIN solicitud s ON (u.correo = s.correo_usuario_emisor OR u.correo = s.correo_usuario_receptor) ' \
+                    'WHERE  s.estado = \'aceptada\' ' \
+                    'GROUP BY u.correo, s.correo_usuario_receptor, s.correo_usuario_emisor ORDER BY amigos DESC) todos ' \
+                    "WHERE todos.correo = '{}'" \
+                    'ORDER BY todos.correo DESC'
 
-def OtrasPublicaciones(conn):
-    pass
+EMPRESAS_POSTULADAS = "select distinct t.id_empresa " \
+                      "from postulacion p, trabajo t " \
+                      "where p.id_trabajo = t.id " \
+                      "and p.correo_usuario = '{}'"
+
+EMPRESAS_TRABAJADAS = "select distinct t.id_empresa from trabajado td, perfil pf, trabajo t " \
+                        "where pf.correo_usuario = '{}' " \
+                        "and td.id_perfil = pf.id " \
+                        "and t.id = td.id_trabajo"
+
+PUBLICACIONES_EMPRESAS = "select * from publicacion where id_empresa = {}"
+PUBLICACIONES_AMIGOS = "select * from publicacion where correo_usuario = '{}'"
+INFO_PUBLICACION_ID = "select p.texto, p.foto, p.link, p.estado, p.fecha, c.contenido " \
+                      "from publicacion p, comentario c where c.id_publicacion = p.id " \
+                        "and p.id = {}"
+
+
+u = "Mono1Apellido1@gmail.com"
+
+def OtrasPublicaciones(usuario, conn):
+    ImprimirTitulo("otras publicaciones")
+    cur = conn.cursor()
+    cur.execute(CONTACTOS_USUARIO.format(usuario))
+    amigos = cur.fetchall()
+    lista_amigos = []
+    for am in amigos:
+        lista_amigos.append(am[0])
+    ids_empresas = []
+    cur.execute(EMPRESAS_POSTULADAS.format(usuario))
+    empresas_postuladas = cur.fetchall()
+    for ep in empresas_postuladas:
+        ids_empresas.append(ep[0])
+    cur.execute(EMPRESAS_TRABAJADAS.format(usuario))
+    empresas_trabajadas = []
+    for et in empresas_trabajadas:
+        if et[0] not in ids_empresas:
+            ids_empresas.append(et[0])
+
+    atributos_publicacion_empresa = [["PUBLICACION", "REALIZADA",  "POR"], ["        ", "Empresa", "Amigo"]]
+    ids_publicaciones = []
+    for id in ids_empresas:
+        cur.execute(PUBLICACIONES_EMPRESAS.format(id))
+        res = cur.fetchall()
+        for r in res:
+            ids_publicaciones.append(r[0])
+            atributos_publicacion_empresa.append([r[0], r[2], ""])
+    for amigo in lista_amigos:
+        cur.execute(PUBLICACIONES_AMIGOS.format(amigo))
+        res = cur.fetchall()
+        for r in res:
+            ids_publicaciones.append(r[0])
+            atributos_publicacion_empresa.append([r[0], "", r[1]])
+    Imprimir(tabulate(atributos_publicacion_empresa))
+    id_elegido = ValidarOpcion(ids_publicaciones, "Ingrese la publicacion que quiera ver en detalle: ")
+    cur.execute(INFO_PUBLICACION_ID.format(id_elegido))
+    info = cur.fetchall()
+    info_publicacion = [["TEXTO", "FOTO", "LINK", "ESTADO", "FECHA", "COMENTARIOS"]]
+    contador = 0
+    for i in info:
+        if contador == 0:
+            info_publicacion.append([i[0], i[1], i[2], i[3], i[4], i[5]])
+            contador += 1
+        else:
+            info_publicacion.append(["", "", "", "", "", i[5]])
+    Imprimir(tabulate(info_publicacion))
+    Imprimir("Que desea hacer?\n"
+             "\t(1) Comentar\n"
+             "\t(2) Eliminar comentario\n"
+             "\t(3) Volver al menu anterior\n"
+             "\t(4) Salir\n")
+    opcion = ValidarOpcion(range(1, 5))
+    if opcion == 4:
+        if HayConexionBD(conn):
+            conn.close()
+        sys.exit(0)
+    elif opcion == 3:
+        return
+    elif opcion == 1:
+        print("comentar")
+        Imprimir("Que desea comentar?\n"
+                 "\t(1) Un comentario\n"
+                 "\t(2) La publicacion\n")
+        op_comentar = ValidarOpcion(range(1,3))
+        if op_comentar == 1:
+            pass
+            #Comentar()
+    elif opcion == 2:
+        print("eliminar comentario")
+
+OtrasPublicaciones(u, conn)
+
+
